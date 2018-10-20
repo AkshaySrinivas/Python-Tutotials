8baee0b29f7d46c2be732caffe62f0f24f275dab
Exam <exam@ASIETDomain.com>
2018-10-20 15:35:55 +0530

    third commit

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from data import Faculties
 app = Flask(__name__)
+
+getFaculties = Faculties()
 
 @app.route("/")
 def index():
     return render_template('home.html')
 
+@app.route("/faculties")
+def senddata():
+    return render_template('faculties.html',sendFacul=getFaculties)
+
 @app.route("/about")
 def about():
     return '<h1>About Works</h1>'
 
+@app.route('/send',methods=['GET','POST'])
+def send():
+    if(request.method=='POST'):
+        getname = request.form['name']
+        getemail = request.form['email']
+        return render_template('contact.html',name=getname,email=getemail)
+
 if(__name__=='__main__'):
-    app.run(debug='true')
+    app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,23 @@
+def Faculties():
+    faculties=[
+        {
+            'dept':'Computer Science',
+            'name':'Akshay',
+            'mob':'1234567890',
+            'email':'as@fgf.com'
+        },
+        {
+            'dept':'Neural Science',
+            'name':'Dangu',
+            'mob':'9934347890',
+            'email':'afs@fgf.com'
+        },
+        {
+            'dept':'Chemical Science',
+            'name':'Eangg PP',
+            'mob':'3393477890',
+            'email':'solo@fgf.com'
+        }
+    ]
+
+    return faculties 
